e84c8ba69997a022c469935390a7edb1164b1da1
Eirren <edv2019@gmail.com>
2026-04-02 15:14:56 -0700

    Created add_goat function

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include <random> // for random selection :)
+#include "Goat.h"
+using namespace std;
+
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
+
+int select_goat(list<Goat> trip);
+void delete_goat(list<Goat> &trip);
+void add_goat(list<Goat> &trip, string nm[], string clr[]);
+void display_trip(list<Goat> trip);
+int main_menu();
+
+int main() {
+    srand(time(0));
+    bool again;
+    int choice;
+
+    // create Goat list containing goats
+    list<Goat> goatList;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
+    while (fin >> names[i++]);
+    fin.close();
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    i = 0;
+    while (fin1 >> colors[i++]);
+    fin1.close();
+
+    // testing, confirmed working :)
+    choice = main_menu();
+    if (choice == 1) {
+        // add_goat();
+    }
+
+
+
+
+    return 0;
+}
+
+int main_menu() {
+    int choice = -1;
+
+    while (choice < 1 || choice > 4) {
+        cout << "*** GOAT MANAGER 3001 ***" << endl;
+        cout << "[1] Add a goat" << endl;
+        cout << "[2] Delete a goat" << endl;
+        cout << "[3] List goats" << endl;
+        cout << "[4] Quit" << endl;
+        cout << "Choice --> ";
+        cin >> choice;
+        if (choice < 1 || choice > 4) {
+            cout << "Invalid choice. Please input a number between 1 to 4" << endl;
+        }
+
+        if (choice == 1) {
+            cout << "You have chosen to add a goat. " << endl;
+            return choice;
+        }
+        else if (choice == 2) {
+            cout << "You have chosen to delete a goat. " ;
+            return choice;
+        }
+        else if (choice == 3) {
+            cout << "You have chosen to list the goats. " ;
+            return choice;
+        }
+        else if (choice == 4) {
+            cout << "You have chosen to quit. " ;
+            return choice;
+        }
+
+    }
+}
+
+int select_goat(list<Goat> trip) {
+    return -1;
+}
+
+void delete_goat(list<Goat> &trip) {
+
+}
+
+void add_goat(list<Goat> &trip, string nm[], string clr[]) {
+    Goat tmpGoat(nm[rand() % SZ_NAMES], rand() % MAX_AGE + 1, clr[rand() % SZ_COLORS]);
+    trip.push_back(tmpGoat);
+}
+
+void display_trip(list<Goat> trip) {
+
+}
